de9b2993bac4f6ef10fc237982a32eeaec1ee4dc
Tung Pham <phamminhtung111097@gmail.com>
2017-12-06 10:43:47 +0700

    Upload First File

---
+++ PPC Rental 2017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PPC_Rental_2017.Startup))]
+namespace PPC_Rental_2017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
