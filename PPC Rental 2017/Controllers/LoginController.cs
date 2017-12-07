73b19d85e35544921f0befa8e11883cd0c734005
Tung Pham <phamminhtung111097@gmail.com>
2017-12-07 13:32:27 +0700

    Login & Register & About

---
+++ PPC Rental 2017/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPC_Rental_2017.Models;
+
+namespace PPC_Rental.Controllers
+{
+    public class LoginController : Controller
+    {
+        team15Entities db = new team15Entities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            if (Session["UserID"] != null)
+            {
+                var userid = int.Parse(Session["UserID"].ToString());
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var user = db.USERs.FirstOrDefault(x => x.Email == username);
+            if (user != null)
+            {
+                if (user.Password.Equals(password))
+                {
+                    Session["FullName"] = user.FullName;
+                    Session["UserID"] = user.ID;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.mgs = "Tài khoản không tồn tại";
+                }
+
+            }
+            return View();
+        }
+        public ActionResult Logout (int id)
+        {
+            var user = db.USERs.FirstOrDefault(x => x.ID == id);
+            if (user != null) 
+            {
+                Session["FullName"] = null;
+                Session["UserID"] = null;
+
+            }
+            return RedirectToAction("Login","Login");
+        }
+
+    }
+}
---
+++ PPC Rental 2017/Controllers/RegisterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPC_Rental_2017.Models;
+using System.IO;
+
+namespace PPC_Rental.Controllers
+{
+    public class RegisterController : Controller
+    {
+        team15Entities db = new team15Entities();
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(string Name, string Address, string Password, string mail, string Number)
+        {
+            var register = new USER();
+            register.Email = mail;
+            register.FullName = Name;
+            register.Password = Password;
+            //register.Phone = int.Parse(string Number);
+            register.Address = Address;
+
+            //db.U.Add(register);
+            //db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
