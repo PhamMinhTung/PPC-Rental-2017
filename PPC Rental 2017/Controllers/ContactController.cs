0283a3181ea567525c0933a677e2cfba3b073d6b
Tung Pham <phamminhtung111097@gmail.com>
2017-12-14 23:56:49 +0700

    Contact

---
+++ PPC Rental 2017/Controllers/ContactController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
+
+namespace PPC_Rental_2017.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(string Name, string Email, string Address, string Message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderemail = new MailAddress("k21t1team5p1d@gmail.com", "Hỗ trợ");//mail agency
+                    var receiveremail = new MailAddress("phamminhtung111097@gmail.com", "Công ty PPC Rental"); //mail công ty
+
+                    var password = "skyblue1110";// mật khẩu địa chỉ mail   
+                    
+                    var body = "Tên: " + Name + " Email: " + Email + " Tiêu đề: " + Address + " Nội dung: " + Message;
+                    // nội dung tin nhắn
+
+
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderemail.Address, password)
+
+                    };
+
+                    using (var mess = new MailMessage(senderemail, receiveremail)
+                    {
+                        
+                        Body = body
+                    }
+                    )
+                    {
+                        smtp.Send(mess);
+                    }
+                    return RedirectToAction("Index", "home");
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "There are some problem in sending email";
+            }
+            return View();
+        }
+    }
+}
---
+++ PPC Rental 2017/Models/SignIn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace PPC_Rental_2017.Models
+{
+    public class SignIn
+    {
+        [Display(Name = "usename")]
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        public string Email { set; get; }
+
+        [Display(Name ="Password")]
+        [StringLength (15, MinimumLength = 6, ErrorMessage = "Độ dài mật khẩu không hợp lệ")]
+        [Required(ErrorMessage ="Vui lòng nhập mật khẩu")]
+        public string Password { set; get; }
+    }
+}
