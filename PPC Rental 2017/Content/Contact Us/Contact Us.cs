228f0c31d5f4eb0ba070d7706505c4aff79506ef
Tung Pham <phamminhtung111097@gmail.com>
2017-12-15 00:00:21 +0700

    FIX 1

---
+++ PPC Rental 2017/Content/Contact Us/Contact Us.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPC_Rental_2017.Content.Contact_Us
+{
+    public class Contact_Us
+    {
+    }
+}
